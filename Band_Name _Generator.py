30d7761dd167c1e08373e81f3f4b3d65762100eb
Keekeecodemode <kiwismillionaire@gmail.com>
2026-07-18 00:20:53 +0530

    Day 1 - Band Name Generator

---
+++ Band_Name _Generator.py
@@ -0,0 +1,5 @@
+name = input("Enter your name : ")
+print("Hello," + name +  " Welcome to Band Name Generator!")
+city = input("Enter your City name\n")
+pet = input("Enter your pet name\n")
+print("Band name could be " + city + " " + pet)
